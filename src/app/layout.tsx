968d47b787af16b879cdb76c4d1c7da60714a5f9
Nero One <info@nero1.net>
2024-01-31 01:59:07 +0800

    refactor: layout and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,18 @@ export default function RootLayout({
       <body
         className={`${GeistSans.variable} ${GeistMono.variable} selection:bg-orange-500 selection:text-white`}
       >
-        <div className="absolute inset-0 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#888_250%)] pointer-events-none -z-20" />
-        <div className="absolute inset-0 h-full w-full bg-transparent bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] pointer-events-none -z-10" />
+        <Background />
         {children}
       </body>
     </html>
   );
 }
+
+const Background = () => {
+  return (
+    <>
+      <div className="absolute inset-0 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#888_250%)] pointer-events-none -z-20" />
+      <div className="absolute inset-0 h-full w-full bg-transparent bg-[radial-gradient(#ddd_1px,transparent_1px)] [background-size:16px_16px] pointer-events-none -z-10" />
+    </>
+  );
+};
---
+++ src/components/header.tsx
@@ -0,0 +1,46 @@
+import { BookOpen, Github } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="flex flex-row justify-between items-center w-full text-lg">
+      <div className="text-xl flex items-center justify-around gap-2">
+        <Link href="/">
+          <div className="flex flex-row gap-2 items-center justify-center">
+            <img src="/logo.svg" className="w-8 h-8 inline-block" alt="logo" />
+            <span className="">translateMY</span>{" "}
+          </div>
+        </Link>
+        <span className="text-xs hidden sm:inline-flex mt-1">
+          model by{" "}
+          <Link
+            href="https://huggingface.co/mesolitica/translation-t5-small-standard-bahasa-cased-v2"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-1 hover:text-orange-600"
+          >
+            mesolitica
+          </Link>
+        </span>
+      </div>
+      <div className="flex flex-row items-center justify-around gap-8">
+        <Link
+          href="https://github.com/0xn1/translate-my"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-orange-600"
+        >
+          <Github className="w-5 h-5 sm:w-6 sm:h-6" />
+        </Link>
+        <Link href="/about" className="hover:text-orange-600">
+          <BookOpen className="w-5 h-5 sm:w-6 sm:h-6" />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
